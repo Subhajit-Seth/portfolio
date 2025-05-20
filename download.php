56d2830d6b05b531d1154cffd98e71493c1a13d2
Subhajit Seth <121784637+Subhajit-Seth@users.noreply.github.com>
2025-05-20 11:12:20 +0530

    Delete download.php

--- download.php
+++
@@ -1,8 +0,0 @@
-<?php
-// download-cv.php
-$file = 'assets/SUBHAJIT-SETH-CV.pdf';
-header('Content-Type: application/pdf');
-header('Content-Disposition: attachment; filename="SUBHAJIT-SETH-CV.pdf"');
-readfile($file);
-exit;
-?>
